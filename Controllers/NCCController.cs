dd69e76ae3b08e10368be3f56fb53cb765c659e2
Huy Canh <huycanh14@gmail.com>
2019-12-21 19:41:10 +0700

    add view create for Nha cung cap

---
+++ Controllers/NCCController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebQLXeMay.Services;
+
+namespace WebQLXeMay.Controllers
+{
+    public class NCCController : Controller
+    {
+        private readonly INCC _ncc;
+        public NCCController(INCC nCC)
+        {
+            _ncc = nCC;
+        }
+        public IActionResult Index(int page = 1, string keyword = "")
+        {
+            var model = _ncc.GetNCCs(page, keyword);
+            ViewBag.keyword = keyword;
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
